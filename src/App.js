a6f896f02d623bfcd275c69933b7380d85237c06
sachinchouksey017 <sachinchouksey017@gmail.com>
2020-11-24 18:33:36 +0530

    Added routing for home component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import Home from './components/home/home'
+// import react router
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 class App extends React.Component {
   constructor() {
@@ -10,7 +12,13 @@ class App extends React.Component {
   render() {
     return (
       <div className="app-main">
-        <Home />
+        <Router>
+          <Switch>
+            <Route path="">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
       </div>
     );
   }
