2dbedc337f8b55b394c8d14018c44bc4dbf055a2
sachinchouksey017 <sachinchouksey017@gmail.com>
2020-11-24 17:02:37 +0530

    Added hello username from bridgelabz

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import logo from './assets/img/BL-logo.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  url = 'https://www.bridgelabz.com/'
+  constructor() {
+    super()
+    this.state = {
+      userName: '',
+      nameError: ''
+    }
+  }
+
+  // onClick function
+  onClick = ($event) => {
+    console.log("save button is clicked!", $event);
+    window.open(this.url, "_blank");
+  }
+  onNameChange = (event) => {
+    console.log("value is ", event.target.value);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    // set the title using setState method
+    this.setState({ userName: event.target.value })
+    if (nameRegex.test(event.target.value)) {
+      this.setState({ nameError: '' })
+    } else {
+      this.setState({ nameError: 'Name is Incorrect' })
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Hello {this.state.userName} from Bridgelabz</h1>
+          <img src={logo} onClick={this.onClick}
+            alt="The Bridgelabz logo: a Bridge to Employment through lab works" />
+        </div>
+        <div className="text-box">
+          <input onChange={this.onNameChange} />
+          <span className="error-output">{this.state.nameError}</span>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+// index.js- this is the javascript file
+
+// Importing core module of React
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// ReactDom help to render react component in a Dom
 ReactDOM.render(
   <React.StrictMode>
     <App />
